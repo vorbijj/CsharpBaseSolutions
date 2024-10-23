f23dfb939e1ebe36126e9be980ab509176a483b6
Ilya Vorobyev <vorbinn@list.ru>
2024-10-23 18:38:34 +0300

    Реализация Lesson 12

---
+++ Lesson12/Task121/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task121
+{
+    internal static class Circle
+    {
+        public static double GetCircumference(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public static double GetAreaCircle(double radius)
+        {
+            return Math.PI * (radius * radius);
+        }
+
+        public static string IsPointBelongsToCircle(double radius, Point centerCircle, Point currentPoint)
+        {
+            string result;
+            double coordinateDifference = Math.Pow(currentPoint.X - centerCircle.X, 2.0) + Math.Pow(currentPoint.Y - centerCircle.Y, 2.0);
+            double squareRadius = radius * radius;
+
+            if (coordinateDifference < squareRadius || Math.Abs(coordinateDifference - squareRadius) < 0.00001)
+            {
+                result = "ПРИНАДЛЕЖИТ";
+            }
+            else
+            {
+                result = "НЕ ПРИНАДЛЕЖИТ";
+            }
+            return $"Точка с координатами ({currentPoint.X}; {currentPoint.Y}) {result} кругу с радиусом ({radius}) и координатами центра ({centerCircle.X}; {centerCircle.Y})";
+        }
+    }
+}
---
+++ Lesson12/Task121/Program12_1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task121
+{
+    //Разработать статический класс для работы с окружностью
+    internal class Program12_1
+    {
+        static void Main(string[] args)
+        {
+            bool isRepeat;
+
+            do
+            {
+                try
+                {
+                    Console.Write("Введите код операции:" +
+                                "\n\t1 - определить длину окружности по заданному радиусу" +
+                                "\n\t2 - определить площадь круга по заданному радиусу" +
+                                "\n\t3 - проверить принадлежит точка с координатами (x,y) кругу с радиусом r и координатами центра (x0,y0)" +
+                                "\nВаш выбор: ");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    if (n <= 0)
+                    {
+                        throw new Exception("Внимание! Введите положительное число, больше нуля!");
+                    }
+                    Calculate(n);
+                    isRepeat = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка! Входная строка имела неверный формат.");
+                    Console.WriteLine();
+                    isRepeat = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                    isRepeat = true;
+                }
+            }
+            while (isRepeat);
+            Console.ReadKey();
+        }
+
+        private static void Calculate(int n)
+        {
+            Console.WriteLine();
+            Console.Write("Введите радиус:\n\tr = ");
+            double radius = GetInputData(true);
+
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine($"Длина окружности по заданному радиусу = {Circle.GetCircumference(radius)}");
+                    break;
+                case 2:
+                    Console.WriteLine($"Площадь круга по заданному радиусу = {Circle.GetAreaCircle(radius)}");
+                    break;
+                case 3:
+                    Point centerCircle = new Point();
+                    Point currentPoint = new Point();
+
+                    Console.WriteLine("Введите координаты центра круга:");
+                    Console.Write("\tx0 = ");
+                    centerCircle.X = GetInputData();
+                    Console.Write("\ty0 = ");
+                    centerCircle.Y = GetInputData();
+
+                    Console.WriteLine("Введите координаты  точки:");
+                    Console.Write("\tx = ");
+                    currentPoint.X = GetInputData();
+                    Console.Write("\ty = ");
+                    currentPoint.Y = GetInputData();
+
+                    Console.WriteLine(Circle.IsPointBelongsToCircle(radius, centerCircle, currentPoint));
+                    break;
+                default:
+                    throw new Exception("Нет операции с указанным номером.");
+            }
+        }
+
+        private static double GetInputData(bool isOnlyPositive = false)
+        {
+            double value = Convert.ToDouble(Console.ReadLine());
+
+            if (isOnlyPositive && value <= 0)
+            {
+                throw new Exception("Внимание! Введите положительное число, больше нуля!");
+            }
+
+            return value;
+        }
+    }
+}

063563134b6a3a396ea31536deef78eb7632c933
Ilya Vorobyev <vorbinn@list.ru>
2024-10-25 17:35:04 +0300

    Реализация Lesson 13

---
+++ Lesson13/Task131/Building.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task131
+{
+    internal class Building
+    {
+        string address;
+        double length;
+        double width;
+        double height;
+
+        public Building(string address, double length, double width, double height)
+        {
+            Address = address;
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public string Address { get => address; set => address = value; }
+        public double Length
+        {
+            get => length;
+            set
+            {
+                if (value < 0)
+                {
+                    length = 0;
+                }
+                else
+                {
+                    length = value;
+                }
+            }
+        }
+        public double Width
+        {
+            get => width;
+            set
+            {
+                if (value < 0)
+                {
+                    width = 0;
+                }
+                else
+                {
+                    width = value;
+                }
+            }
+        }
+        public double Height
+        {
+            get => height;
+            set
+            {
+                if (value < 0)
+                {
+                    height = 0;
+                }
+                else
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public string Print()
+        {
+            return $"Адрес: {address}" +
+                $"\nДлина: {length}" +
+                $"\nШирина: {width}" +
+                $"\nВысота: {height}";
+        }
+    }
+}
---
+++ Lesson13/Task131/Program13_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task131
+{
+    internal class Program13_1
+    {
+        static void Main(string[] args)
+        {
+            Building baseBuilding = new Building("г.Москва", 12.6, 6, 9.3);
+            MultiBuilding multiBuilding = new MultiBuilding("г.Нижний Новгород", 15.9, 6.3, 6.6, 2);
+            Building extraBuilding = new MultiBuilding("г.Томск", 32.1, 12.3, 15.3, 4);
+
+            Console.WriteLine(baseBuilding.Print());
+            Console.WriteLine();
+            Console.WriteLine(multiBuilding.Print());
+            Console.WriteLine();
+            Console.WriteLine(extraBuilding.Print());
+            Console.ReadKey();
+        }
+    }
+}

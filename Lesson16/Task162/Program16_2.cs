c9330961918444dd2a2dbc4fb2564b5f0e579940
Ilya Vorobyev <vorbinn@list.ru>
2024-10-30 18:20:30 +0300

    Реализация Lesson 16

---
+++ Lesson16/Task162/Program16_2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Task162
+{
+    //Программа для получения информации о товаре из json-файла
+    internal class Program16_2
+    {
+        static void Main(string[] args)
+        {
+            string nameFile = "Products.json";
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), nameFile);
+
+            string jsonString = string.Empty;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                jsonString = sr.ReadToEnd();
+
+            }
+
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+
+            Product product = products[0];
+            foreach (Product p in products)
+            {
+                Console.WriteLine(p.ToString());
+
+                if (product.Price < p.Price)
+                {
+                    product = p;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Самый дорогой товар:");
+            Console.WriteLine(product.ToString());
+            Console.ReadKey();
+        }
+    }
+}

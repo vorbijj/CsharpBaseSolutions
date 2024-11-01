977f218864aea795b0a033ac3aae1d4c3fa794e1
Ilya Vorobyev <vorbinn@list.ru>
2024-11-01 17:17:35 +0300

    Реализация Lesson 17

---
+++ Lesson17/Task171/BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task171
+{
+    internal class BankAccount<T>
+    {
+        T account;
+        string name;
+        double balance;
+
+        public void Input()
+        {
+            bool isRepeat;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Введите данные счета:");
+                    Console.Write("\tномер счета = ");
+                    account = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+
+                    Console.Write("\tимя владельца = ");
+                    name = Console.ReadLine();
+
+                    Console.Write("\tбаланс = ");
+                    balance = Convert.ToDouble(Console.ReadLine());
+
+                    isRepeat = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                    isRepeat = true;
+                }
+            }
+            while (isRepeat);
+        }
+
+        public string GetInfo()
+        {
+            return $"Информация о банковском счете:" +
+                        $"\n\tномер счет -> {account}" +
+                        $"\n\tимя владельца -> {name}" +
+                        $"\n\tбаланс -> {balance}\n";
+        }
+    }
+}

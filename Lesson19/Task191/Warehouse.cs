093a8c32dc6cdd412c404d03f9ed628c6971db96
Ilya Vorobyev <vorbinn@list.ru>
2024-11-05 14:43:17 +0300

    Реализация Lesson 19

---
+++ Lesson19/Task191/Warehouse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task191
+{
+    internal class Warehouse
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string ProcessorType { get; set; }
+        public int ProcessorFrequency { get; set; }
+        public int AmountRAM { get; set; }
+        public int VolumeHardDisk { get; set; }
+        public int AmountMemoryVideoCard { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"код ({Code}) -> наименование ({Name}) -> тип процессора ({ProcessorType}) -> частота процессора ({ProcessorFrequency})" +
+                $" -> ОЗУ ({AmountRAM}) -> Диск ({VolumeHardDisk}) -> GPU ({AmountMemoryVideoCard}) -> цена ({Price}) -> кол-во ({Quantity})";
+        }
+    }
+}
